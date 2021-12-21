b10937a83d9359eb32fd38ff50255a5dcb0af5eb
anna-hayoun <anna.hayoun@laplateforme.io>
2021-12-21 21:46:39 +0100

    config SQL

--- sqlconfig.php
+++ sqlconfig.php
@@ -1,7 +1,12 @@
 <?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'moduleconnexion');
  
 // Connexion à la base de données MySQL 
-$conn = mysqli_connect("localhost", "root", "", "moduleconnexion");
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Vérifier la connexion
 if($conn === false)
